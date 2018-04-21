1c7e679f37fcc94ddd72833591ae771aa92e1994
Alice Creasey <alice.creasey@hotmail.com>
2018-04-21 18:35:10 +0100

    Home-hero banner
    Hero banner now shows latest post

--- routes.php
+++ routes.php
@@ -32,7 +32,7 @@
   // we consider those "allowed" values
   $controllers = array('pages' => ['home', 'error', 'account', 'adminAccount'], 
                        'users' => ['register', 'login', 'logout'],
-                       'posts' => ['showAll', 'showPost', 'searchPost', 'createPost', 'update', 'delete']);
+                       'posts' => ['showAll', 'showPost', 'searchPost', 'createPost', 'update', 'delete', 'populateHomepage']);
 
   // check that the requested controller and action are both allowed
   // if someone tries to access something else he will be redirected to the error action of the pages controller
