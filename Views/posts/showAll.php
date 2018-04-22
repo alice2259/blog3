03c7f8fce12db4db921340d23fd653c50b8d28e1
JbYorks <jennabrownuk@gmail.com>
2018-04-22 14:20:28 +0100

    Delete
    completely works now - plus modal

--- Views/posts/showAll.php
+++ Views/posts/showAll.php
@@ -23,23 +23,46 @@
           <b class="galleryCaption"><?php echo $post->title ?></b>
           <br>
         <div class="row flex-auto ml-0">
-       
+      </a>   
 <!--HIDDEN BUTTONS DEPENDANT ON USER ACCESS-->
             <?php
               if(isset($_SESSION["firstName"])) { 
                   if($_SESSION['permissionsID']==1) {
               ?> <div class="col-lg-2 ml-0 my-2"><a class="btn btn-secondary btn-sm" href="?controller=posts&action=updatePost&id=<?php echo $post->postID ?>" role="button">Update</a></div>
               <?php   } else { ?>
-              <div class="ml-1 my-2"><a class="btn btn-secondary btn-sm text-white" href="?controller=posts&action=deletePost&id=<?php echo $post->postID ?>" role="button">Delete</a></div>
+            
+              <button type="button" class="btn btn-primary btn-sm ml-1 my-2" data-toggle="modal" data-target="#deleteModal">Delete</button>
               <div class="ml-1 my-2"><a class="btn btn-secondary btn-sm text-white" href="?controller=posts&action=updatePost&id=<?php echo $post->postID ?>" role="button">Update</a></div>
               <?php } } ?>
               
+
+     <!-- DELETE MODAL TO CHECK USER DEFINITELY WANT TO DELETE -->         
+              
+              <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Caution: Delete Post?</h5>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                      </button>
+                    </div>
+                    <div class="modal-body">
+                        <p>Are you sure you want to delete the post <b><h6>"<?php echo $post->title ?>"</h6></b>?</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button type="button" class="btn btn-primary" onclick="location.href='?controller=posts&action=deletePost&id=<?php echo $post->postID ?>'">Yes, delete</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
         </div>
       </div>
     </a>
  
 
-     <?php } ?> <!-- end of for each-->
+     <?php } ?> <!-- end of foreach-->
     </div>
   </div>
 </div>
